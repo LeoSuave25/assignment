88b2b7339d45215c218daaf04a509780d2b96b9a
LeoSuave25 <129582854+LeoSuave25@users.noreply.github.com>
2023-04-01 21:19:43 +0800

    Update ProblemTwo.py

--- ProblemTwo.py
+++ ProblemTwo.py
@@ -4,15 +4,26 @@ title = "Decryption Tool"
 print("\033[32m",fig.figlet_format(title),"\033[m")
 print(fig.figlet_format("Made by: Leoj M. Suaverdez",font="bubble"))
 # User enter the encrypted text to decrypt
-encrypted_text = str(input("Enter the encrypted text: "))
+while True:
+    encrypted_text = str(input("Enter the encrypted text: "))
 
-# Decrypt the input text by replacing each encrypted character with its original vowel
-decrypted_text = encrypted_text.replace("*","a") # Replace "*" with "a"
-decrypted_text = decrypted_text.replace("&","e") # Replace "&" with "e"
-decrypted_text = decrypted_text.replace("#","i") # Replace "#" with "i"
-decrypted_text = decrypted_text.replace("+","o") # Replace "+" with "o"
-decrypted_text = decrypted_text.replace("!","u") # Replace "!" with "u"
+    # Decrypt the input text by replacing each encrypted character with its original vowel
+    decrypted_text = encrypted_text.replace("*","a") # Replace "*" with "a"
+    decrypted_text = decrypted_text.replace("&","e") # Replace "&" with "e"
+    decrypted_text = decrypted_text.replace("#","i") # Replace "#" with "i"
+    decrypted_text = decrypted_text.replace("+","o") # Replace "+" with "o"
+    decrypted_text = decrypted_text.replace("!","u") # Replace "!" with "u"
 
-# Print the decrypted text
-print("\033[32m",decrypted_text,"\033[m")
-print("\033[32m","="*100,"\033[m")
+    # Print the decrypted text
+    print("\033[32m",decrypted_text,"\033[m")
+    print("\033[32m","="*100,"\033[m")
+    while True:
+        response = str(input("Do you want to try again? [Y/N]"))
+        if response == "N":
+            print("Program will now Exit")
+            exit()
+        elif response == "Y":
+            print("Decrypt a text again")
+            break
+        else:
+            print("Invalid response. Please enter Y or N.")
